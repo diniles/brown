7a2e8055b1d3740ca8edfdfe38caf096d150db9a
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-03 21:26:11 +0300

    Add: routes to static files

--- preliminary/helloworld.js
+++ preliminary/helloworld.js
@@ -1,21 +1,33 @@
 const http = require("http");
+const fs = require("fs");
 const port = process.env.PORT || 3000;
+
+function serveStaticFile(res, path, contentType, responseCode = 200) {
+  fs.readFile(__dirname + path, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      return res.end("500 - Internal Error");
+    }
+    res.writeHead(responseCode, { "Content-Type": contentType });
+    res.end(data);
+  });
+}
 
 const server = http.createServer((req, res) => {
   // url to lower case and remove / in the end of line
   const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
   switch (path) {
     case "":
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Homepage");
+      serveStaticFile(res, "public/home.html", "text/html");
       break;
     case "/about":
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("About");
+      serveStaticFile(res, "public/about.html", "text/html");
+      break;
+    case "/img/logo.png":
+      serveStaticFile(res, "public/img/logo.png", "image/png");
       break;
     default:
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Not Found");
+      serveStaticFile(res, "public/404.html", "text/html");
       break;
   }
 });
