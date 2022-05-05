b09775af78af9f13c6a96a3fb7e0532ef9137de4
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-05 23:29:44 +0300

    Add: fix eslint errors

--- meadowlark/lib/__tests__/handler.test.js
+++ meadowlark/lib/__tests__/handler.test.js
@@ -1,39 +1,39 @@
-const handlers = require("../handlers");
+const handlers = require('../handlers');
 
-test("home page renders", () => {
+test('home page renders', () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.home(req, res);
-  expect(res.render.mock.calls[0][0]).toBe("home");
+  expect(res.render.mock.calls[0][0]).toBe('home');
 });
 
-test("Page About with fortunes", () => {
+test('Page About with fortunes', () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.about(req, res);
   expect(res.render.mock.calls.length).toBe(1);
-  expect(res.render.mock.calls[0][0]).toBe("about");
+  expect(res.render.mock.calls[0][0]).toBe('about');
   expect(res.render.mock.calls[0][1]).toEqual(
     expect.objectContaining({
       fortune: expect.stringMatching(/\W/),
-    })
+    }),
   );
 });
 
-test("rendering error handler 404", () => {
+test('rendering error handler 404', () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.notFound(req, res);
   expect(res.render.mock.calls.length).toBe(1);
-  expect(res.render.mock.calls[0][0]).toBe("404");
+  expect(res.render.mock.calls[0][0]).toBe('404');
 });
 
-test("rendering error handler 500", () => {
-  const err = new Error("some error");
+test('rendering error handler 500', () => {
+  const err = new Error('some error');
   const req = {};
   const res = { render: jest.fn() };
   const next = jest.fn();
   handlers.serverError(err, req, res, next);
   expect(res.render.mock.calls.length).toBe(1);
-  expect(res.render.mock.calls[0][0]).toBe("500");
+  expect(res.render.mock.calls[0][0]).toBe('500');
 });
--- meadowlark/lib/handlers.js
+++ meadowlark/lib/handlers.js
@@ -2,8 +2,7 @@ const fortune = require('./fortune');
 
 exports.home = (req, res) => res.render('home');
 
-exports.about = (req, res) =>
-  res.render('about', {fortune: fortune.getFortune()});
+exports.about = (req, res) => res.render('about', { fortune: fortune.getFortune() });
 
 exports.notFound = (req, res) => res.render('404');
 
