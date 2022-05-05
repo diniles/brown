abaac6461fa8e6f8264acf430203e606b4748c9d
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-05 22:43:51 +0300

    Fix: errors in integration test for page "About"

--- meadowlark/integration-tests/basic-navigation.test.js
+++ meadowlark/integration-tests/basic-navigation.test.js
@@ -1,7 +1,7 @@
 const portfinder = require('portfinder')
 const puppeteer = require('puppeteer')
 
-const app = require('../meadowlark')
+const app = require('../meadowlark.js')
 
 let server = null
 let port = null
@@ -15,14 +15,14 @@ afterEach(() => {
   server.close()
 })
 
-test('homepage links to page About', async () => {
+test('home page links to about page', async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(`http://localhost:${port}`)
   await Promise.all([
     page.waitForNavigation(),
-    page.click('[date-test-id="about"]'),
+    page.click('[data-test-id="about"]'),
   ])
   expect(page.url()).toBe(`http://localhost:${port}/about`)
-  await browser.close
+  await browser.close()
 })
